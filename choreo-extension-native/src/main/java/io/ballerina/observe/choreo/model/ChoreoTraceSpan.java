f734adbe677c78b9e3b96cf8d07b7cc099ca49b1
sachiniSam <sachiniprof@gmail.com>
2021-01-15 14:19:19 +0530

    Add checkpoint support to choreo extension

--- choreo-extension-native/src/main/java/io/ballerina/observe/choreo/model/ChoreoTraceSpan.java
+++ choreo-extension-native/src/main/java/io/ballerina/observe/choreo/model/ChoreoTraceSpan.java
@@ -1,17 +1,19 @@
 /*
- * Copyright (c) 2020, WSO2 Inc. (http://wso2.com) All Rights Reserved.
+ * Copyright (c) 2020, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
  *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
  */
 
 package io.ballerina.observe.choreo.model;
@@ -33,9 +35,11 @@ public class ChoreoTraceSpan {
     private long duration;
     private Map<String, String> tags;
     private List<Reference> references;
+    private List<SpanEvent> events;
 
     public ChoreoTraceSpan(long traceId, long spanId, String serviceName, String operationName,
-                           long timestamp, long duration, Map<String, String> tags, List<Reference> references) {
+                           long timestamp, long duration, Map<String, String> tags, List<Reference> references,
+                           List<SpanEvent> events) {
         this.traceId = traceId;
         this.spanId = spanId;
         this.serviceName = serviceName;
@@ -44,6 +48,7 @@ public class ChoreoTraceSpan {
         this.duration = duration;
         this.tags = tags;
         this.references = references;
+        this.events = events;
     }
 
     public long getTraceId() {
@@ -76,6 +81,10 @@ public class ChoreoTraceSpan {
 
     public List<Reference> getReferences() {
         return references;
+    }
+
+    public List<SpanEvent> getEvents() {
+        return events;
     }
 
     /**
