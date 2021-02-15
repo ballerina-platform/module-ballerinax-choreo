e0a059d4661219c94146a7e19b0777166078b2d6
Nadun De Silva <nadunrds@gmail.com>
2021-02-15 17:07:59 +0530

    Add periscope services to test suite startup and cleanup

--- choreo-extension-tests/src/test/java/io/ballerina/observe/choreo/BaseTestCase.java
+++ choreo-extension-tests/src/test/java/io/ballerina/observe/choreo/BaseTestCase.java
@@ -17,11 +17,14 @@
  */
 package io.ballerina.observe.choreo;
 
+import org.ballerinalang.test.context.BServerInstance;
 import org.ballerinalang.test.context.BalServer;
 import org.ballerinalang.test.context.BallerinaTestException;
+import org.ballerinalang.test.context.Utils;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -35,15 +38,28 @@ import java.util.logging.Logger;
  */
 public class BaseTestCase {
     private static final Logger LOGGER = Logger.getLogger(BaseTestCase.class.getName());
+    static final File RESOURCES_DIR = Paths.get("src", "test", "resources", "bal").toFile();
+
+    private BServerInstance periscopeBackendServerInstance;
     static BalServer balServer;
 
     @BeforeSuite(alwaysRun = true)
     public void initialize() throws BallerinaTestException {
         balServer = new BalServer();
+
+        periscopeBackendServerInstance = new BServerInstance(balServer);
+        final String projectDir = Paths.get(RESOURCES_DIR.getAbsolutePath(), "choreo_periscope_backend").toFile()
+                .getAbsolutePath();
+        int[] requiredPorts = {10090};
+        periscopeBackendServerInstance.startServer(projectDir, "choreo_periscope_backend-0.1.0", null, null,
+                requiredPorts);
+        Utils.waitForPortsToOpen(requiredPorts, 1000 * 60, false, "localhost");
     }
 
     @AfterSuite(alwaysRun = true)
-    public void destroy() throws IOException {
+    public void destroy() throws IOException, BallerinaTestException {
+        periscopeBackendServerInstance.shutdownServer();
+
         Path ballerinaInternalLog = Paths.get(balServer.getServerHome(), "ballerina-internal.log");
         if (Files.exists(ballerinaInternalLog)) {
             LOGGER.severe("=== Ballerina Internal Log Start ===");
