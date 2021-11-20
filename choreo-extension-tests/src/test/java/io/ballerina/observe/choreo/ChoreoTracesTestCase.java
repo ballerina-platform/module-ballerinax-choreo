a685bd8e41f33c63dba164506428c7661915f3bd
Nadun De Silva <nadunrds@gmail.com>
2021-11-20 11:33:35 +0530

    Fix minor issues

--- choreo-extension-tests/src/test/java/io/ballerina/observe/choreo/ChoreoTracesTestCase.java
+++ choreo-extension-tests/src/test/java/io/ballerina/observe/choreo/ChoreoTracesTestCase.java
@@ -135,7 +135,7 @@ public class ChoreoTracesTestCase extends BaseTestCase {
         Map<String, String> env = new HashMap<>(additionalEnvVars);
         env.put("BAL_CONFIG_FILES", configFile);
 
-        // Cleaning up Dependencies.toml o avoid dependencies issues in dependency updates
+        // Cleaning up Dependencies.toml to avoid dependency issues in dependency updates
         final String projectDir = Paths.get(RESOURCES_DIR.getAbsolutePath(), "choreo_ext_test").toFile()
                 .getAbsolutePath();
         Files.deleteIfExists(Paths.get(projectDir, "Dependencies.toml"));
